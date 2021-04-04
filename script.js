3bbefb53e8f0e3e7eff8b771db9d6ca97aef2d6e
Chris Rebuck <crebuck.apt@gmail.com>
2021-04-04 15:05:28 -0500

    Completed the Week8B Studio but did not do Bonus Mission 1 & 2

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+        response.json().then(
+            function(json) {
+                console.log(json);
+                
+
+                function astronautTemplate(astronauts) {
+                    return ` 
+                    <div class = "astronaut">
+                        <div class="bio">
+                            <h3>${astronauts.firstName} ${astronauts.lastName}</h3>
+                                <ul>
+                                    <li> Hours in space: ${astronauts.hoursInSpace} </li>
+                                    <li>Active: ${astronauts.active}</li>
+                                    <li>Skills: ${astronauts.skills.join(', ')}</li>
+                                </ul>
+                        </div>       
+                        <img class="avatar" src=${astronauts.picture}>
+                    </div>
+                    `
+
+                }
+                const groupOfAstronauts = document.getElementById("container").innerHTML =
+                    `${json.map(astronautTemplate)}`
+                    
+            
+                const footer = document.getElementById("footer")
+                footer.innerHTML = `Number of astronauts displayed: ${json.length}` 
+            })
+       
+   
+    });
+
+         
+})
